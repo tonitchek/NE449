e35a6d7637c994c9c4a6ee5d27b60b1189dea0a5
broquet <broquet@esrf.fr>
2016-10-02 22:14:15 +0200

    add sync of all threads. Sync is not done in main but by threads themselves. Thread that is to be expected is passed in constructor

--- src/tdm7Ex3/TaskForce.java
+++ src/tdm7Ex3/TaskForce.java
@@ -3,25 +3,53 @@ package tdm7Ex3;
 public class TaskForce extends Thread {
 
 	private int counter;
-	
-	TaskForce(String name,int counter) {
-		super(name);
-		this.counter = counter;
-	}
+	private boolean severalThreads;
+	private TaskForce th;
+	private TaskForce[] ths;
+	private int thNb;
+	private long waiting;
 	
 	TaskForce(String name, TaskForce th, int counter) {
 		super(name);
 		this.counter = counter;
-		try {
-			th.join();
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		this.severalThreads = false;
+		this.th = th;
+		waiting = (long) (Math.random()*1000);
+		System.out.println(this.getName()+" waiting time: "+waiting+"ms");
+	}
+	
+	TaskForce(String name, TaskForce[] th, int thNb, int counter) {
+		super(name);
+		this.counter = counter;
+		this.severalThreads = true;
+		this.ths = th;
+		this.thNb = thNb;
+		waiting = (long) (Math.random()*1000);
+		System.out.println(this.getName()+" waiting time: "+waiting+"ms");
 	}
 	
 	public void run() {
-		long waiting = (long) (Math.random()*1000);
+		if(severalThreads) {
+			try {
+				for(int i=0;i<thNb;i++) {
+					ths[i].join();
+				}
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		else {
+			try {
+				if(th != null) {
+					th.join();
+				}
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
 		for(int i=0;i<counter;i++) {
 			try {
 				Thread.sleep(waiting);
--- src/tdm7Ex3/ThreadSync.java
+++ src/tdm7Ex3/ThreadSync.java
@@ -4,10 +4,22 @@ public class ThreadSync {
 
 	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
-		TaskForce th1 = new TaskForce("Task1",10);
+		TaskForce th1 = new TaskForce("Task1",null,10);
 		th1.start();
 		TaskForce th2 = new TaskForce("Task2",th1,3);
 		th2.start();
+		TaskForce th3 = new TaskForce("Task3",th1,8);
+		th3.start();
+		TaskForce th4 = new TaskForce("Task4",th1,5);
+		th4.start();
+		TaskForce[] th23 = new TaskForce[2];
+		th23[0] = th2;
+		th23[1] = th3;
+		TaskForce th5 = new TaskForce("Task5",th23,2,12);
+		th5.start();
+		TaskForce th6 = new TaskForce("Task6",th4,6);
+		th6.start();
+		System.out.println("All threads started... Let's syncing");
 	}
 
 }
